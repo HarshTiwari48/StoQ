0943dbeb86c0036a6a192185ce719bc956930846
Harsh Tiwari <tiwari.harsh.p@gmail.com>
2026-04-03 22:52:10 +0530

    newsApi and mlConncetion added

---
+++ client-server/src/lib/mlService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const ML_SERVICE_URL = process.env.ML_SERVICE_URL!;
+
+export async function sendNewsToML(newsData: any[]) {
+  try {
+    const mlPayload = newsData.map((item) => ({
+      date: item.date,
+      source: item.source,
+      title: item.title,
+      description: item.description,
+    }));
+
+    console.log("Sending to ML:", mlPayload.length);
+
+    const response = await axios.post(
+      ML_SERVICE_URL,
+      mlPayload
+    );
+
+    return response.data;
+  } catch (error: any) {
+    console.error(
+      "ML API ERROR:",
+      error?.response?.data || error.message
+    );
+    throw error;
+  }
+}
---
+++ client-server/src/lib/newsService.ts
@@ -0,0 +1,50 @@
+export async function fetchFinanceNews() {
+  try {
+    const fromDate = new Date(
+      Date.now() - 3 * 24 * 60 * 60 * 1000
+    )
+      .toISOString()
+      .split("T")[0];
+
+    const toDate = new Date()
+      .toISOString()
+      .split("T")[0];
+
+    const query = `
+      "Nifty" OR "Sensex" OR "NSE" OR "BSE" OR "Reliance" OR "Tata" 
+      OR "Infosys" OR "HDFC" OR "ICICI" OR "ITC" OR "Maruti" OR "banking" OR "stocks" OR "Crude oil" OR "Pharma" OR "Profit" OR "Market"
+    `;
+
+    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(
+      query
+    )}&from=${fromDate}&to=${toDate}&sortBy=publishedAt&language=en&pageSize=15&apiKey=${process.env.NEWS_API_KEY}`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log("Fetched articles:", data.articles?.length);
+
+    return data.articles
+      .filter(
+        (article: any) =>
+          article.title &&
+          article.title !== "[Removed]" &&
+          article.description
+      )
+      .map((article: any, index: number) => ({
+        article_id: index + 1,
+        date: article.publishedAt?.slice(0, 10),
+        source: article.source?.name || "Unknown",
+        title: article.title,
+        description: article.description,
+        source_url: article.url,
+        image_url: article.urlToImage,
+        author: article.author,
+        published_at: article.publishedAt,
+      }));
+  } catch (error) {
+    console.error("News fetch failed:", error);
+    throw error;
+  }
+}
